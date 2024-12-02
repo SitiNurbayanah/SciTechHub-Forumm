3d0fb77cd9406fb9dfb20dc704b186c625514ebc
IBayyy <119025967+SitiNurbayanah@users.noreply.github.com>
2024-12-02 08:09:29 +0700

    Update HomeController.php

--- app/Http/Controllers/Pages/HomeController.php
+++ app/Http/Controllers/Pages/HomeController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers\Pages;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Thread;
 
 class HomeController extends Controller
 {
     public function index()
-    {
-        return view('home.index');
+    {   
+        return view('pages.threads.index', [
+            'threads'       => Thread::orderBy('id', 'desc')->paginate(10),
+        ]);
     }
 }
